41d81c42289377a89e2b1d0d478646b465051825
Arvin <32810701+Arvin-Dumpling@users.noreply.github.com>
2018-01-12 16:55:17 +0800

    UI制作，模型导入修改

---
+++ Assets/_Script/ShowMachine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMachine : MonoBehaviour
+{
+
+    private GameObject Bts;
+	// Use this for initialization
+	void Awake ()
+	{
+	    //Bts = this.transform.parent.Find("Buttons");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Show()
+    {
+
+
+    }
+}
